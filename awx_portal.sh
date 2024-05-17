a5ec163cd8354a1f557b5fd9537c2091eb0711f4
root <root@kali-evros.home>
2024-05-17 23:49:13 +0100

    newawx

--- awx_portal.sh
+++ awx_portal.sh
@@ -3,22 +3,37 @@
 # Set the namespace where AWX is deployed
 NAMESPACE="awx"
 
+# Find the AWX deployment
+AWX_DEPLOYMENT=$(kubectl get deployments -n $NAMESPACE -o jsonpath='{.items[0].metadata.name}')
+
+if [ -z "$AWX_DEPLOYMENT" ]; then
+  echo "No AWX deployment found in the $NAMESPACE namespace."
+  exit 1
+fi
+
+echo "Found AWX deployment: $AWX_DEPLOYMENT"
+
 # Check if the awx-service already exists
 SERVICE_EXISTS=$(kubectl get service awx-service -n $NAMESPACE --ignore-not-found)
 
 # Expose the AWX service if it does not exist
 if [ -z "$SERVICE_EXISTS" ]; then
   echo "Exposing AWX service..."
-  kubectl expose deployment awx --type=NodePort --name=awx-service -n $NAMESPACE
+  kubectl expose deployment $AWX_DEPLOYMENT --type=NodePort --name=awx-service -n $NAMESPACE
 else
   echo "AWX service already exposed."
 fi
 
 # Get the service details
-SERVICE_DETAILS=$(kubectl get service awx-service -n $NAMESPACE -o jsonpath='{.spec.ports[0].nodePort}')
+SERVICE_PORT=$(kubectl get service awx-service -n $NAMESPACE -o jsonpath='{.spec.ports[0].nodePort}')
 
 # Get the IP address of the first node
 NODE_IP=$(kubectl get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}')
 
 # Display the URL to access the AWX web page
-echo "AWX web page is available at: http://$NODE_IP:$SERVICE_DETAILS"
+if [ -n "$SERVICE_PORT" ] && [ -n "$NODE_IP" ]; then
+  echo "AWX web page is available at: http://$NODE_IP:$SERVICE_PORT"
+else
+  echo "Failed to retrieve service port or node IP."
+fi
+
